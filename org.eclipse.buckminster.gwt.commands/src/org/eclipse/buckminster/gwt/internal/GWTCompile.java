2c979ba4877695a324638a42ca23c123fb6b75af
Dann Martens <me@dannmartens.com>
2011-03-25 16:58:13 +0100

    Populated projects.

---
+++ org.eclipse.buckminster.gwt.commands/src/org/eclipse/buckminster/gwt/internal/GWTCompile.java
@@ -0,0 +1,16 @@
+package org.eclipse.buckminster.gwt.internal;
+
+import org.eclipse.buckminster.core.commands.WorkspaceCommand;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class GWTCompile extends WorkspaceCommand {
+
+	@Override
+	protected int internalRun(IProgressMonitor monitor) throws Exception {
+		
+		
+		
+		return 0;
+	}
+
+}
