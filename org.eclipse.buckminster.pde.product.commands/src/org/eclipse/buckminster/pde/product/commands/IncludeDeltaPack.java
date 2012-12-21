6b6ccabf29fd7e8fa69517505d903bbb475f9fe8
Dann Martens <me@dannmartens.com>
2012-12-21 16:50:38 +0100

    Added support for target update (delta pack).

---
+++ org.eclipse.buckminster.pde.product.commands/src/org/eclipse/buckminster/pde/product/commands/IncludeDeltaPack.java
@@ -0,0 +1,63 @@
+package org.eclipse.buckminster.pde.product.commands;
+
+import java.io.File;
+
+import org.eclipse.buckminster.cmdline.SimpleErrorExitException;
+import org.eclipse.buckminster.cmdline.UsageException;
+import org.eclipse.buckminster.core.commands.WorkspaceCommand;
+import org.eclipse.buckminster.pde.internal.PDETargetPlatform;
+import org.eclipse.buckminster.runtime.Buckminster;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.pde.core.target.ITargetDefinition;
+import org.eclipse.pde.core.target.ITargetHandle;
+import org.eclipse.pde.core.target.ITargetLocation;
+import org.eclipse.pde.core.target.ITargetPlatformService;
+
+
+public class IncludeDeltaPack extends WorkspaceCommand {
+
+	private File deltaPackPath;
+	
+	@Override
+	protected int internalRun(IProgressMonitor monitor) throws Exception {
+		if (deltaPackPath == null)
+			throw new SimpleErrorExitException(org.eclipse.buckminster.core.Messages.Too_few_arguments);
+		if(!deltaPackPath.exists())
+			throw new SimpleErrorExitException("Path does not exist!");
+		if(!deltaPackPath.isDirectory())
+			throw new SimpleErrorExitException("Path is not a directory!");
+		Buckminster bucky = Buckminster.getDefault();
+		ITargetPlatformService service = bucky.getService(ITargetPlatformService.class);		
+		ITargetHandle activeTarget = service.getWorkspaceTargetHandle();
+		ITargetDefinition newTarget = service.newTarget();
+		//? Copy.
+		service.copyTargetDefinition(activeTarget.getTargetDefinition(), newTarget);
+		//? Add Delta Pack directory path.
+		ITargetLocation[] locations = newTarget.getTargetLocations();
+		ITargetLocation[] newLocations = new ITargetLocation[locations.length + 1];
+		System.arraycopy(locations, 0, newLocations, 0, locations.length);
+		newLocations[locations.length] = service.newDirectoryLocation(deltaPackPath.getAbsolutePath());
+		newTarget.setName(activeTarget.getTargetDefinition().getName() + " w/Delta");
+		System.out.println("Added delta pack.");
+		newTarget.setTargetLocations(newLocations);
+		//? SHOULD WE RESOLVE???
+		//newTarget.resolve(monitor);
+		service.saveTargetDefinition(newTarget);
+		PDETargetPlatform.setTargetActive(newTarget, monitor);
+		return 0;
+	}
+
+	@Override
+	protected void handleUnparsed(String[] unparsed) throws Exception {
+		int len = unparsed.length;
+		if(len > 1)
+			throw new UsageException("Too many arguments");
+		if(len == 1)
+			setDeltaPackPath(unparsed[0]);
+	}
+
+	private void setDeltaPackPath(String path) {
+		this.deltaPackPath = new File(path);		
+	}
+
+}
